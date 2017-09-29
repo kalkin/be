kalkin/be

commit d40f713829e8d92fd0b492d2d901223cd94eace4

Add support for submodules

libbe/storage/vcs/git.py
@@ -323,7 +323,7 @@ class ExecGit (PygitGit):
         # dealing with a bare repo.
         if os.path.isdir(path) != True:
             path = os.path.dirname(path)
-        status,output,error = self._u_invoke_client('rev-parse', '--git-dir',
+        status,output,error = self._u_invoke_client('rev-parse', '--show-top-level',
                                                     cwd=path)
         gitdir = os.path.join(path, output.rstrip('\n'))
         dirname = os.path.abspath(os.path.dirname(gitdir))

commit b6873606c566d9335cfe0f8fa347adf2764b7b32

Fix bug introduced by d40f713

libbe/storage/vcs/git.py
@@ -324,11 +324,9 @@ class ExecGit (PygitGit):
         # dealing with a bare repo.
         if os.path.isdir(path) != True:
             path = os.path.dirname(path)
-        status,output,error = self._u_invoke_client('rev-parse', '--show-top-level',
-                                                    cwd=path)
-        gitdir = os.path.join(path, output.rstrip('\n'))
-        dirname = os.path.abspath(os.path.dirname(gitdir))
-        return dirname
+        _, output, __ = self._u_invoke_client('rev-parse', '--show-toplevel',
+                                              cwd=path)
+        return os.path.abspath(output.strip())
 
     def _vcs_init(self, path):
         self._u_invoke_client('init', cwd=path)