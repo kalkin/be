64129be4eaab8df49b29abd010bfcb980ff574f6
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2017-09-30 16:38:24 +0200

    Fix Depend imports

--- libbe/command/depend.py
+++ libbe/command/depend.py
@@ -18,10 +18,6 @@
 # You should have received a copy of the GNU General Public License along with
 # Bugs Everywhere.  If not, see <http://www.gnu.org/licenses/>.
 
-import copy
-import itertools
-import os
-
 import libbe
 import libbe.bug
 import libbe.bugdir
