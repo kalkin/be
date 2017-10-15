c00815811ec7c12486993dd1a401d567a461744b
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2017-10-15 20:41:42 +0200

    Port diff command to docopt

--- libbe/command/diff.py
+++ libbe/command/diff.py
@@ -18,6 +18,26 @@
 # You should have received a copy of the GNU General Public License along with
 # Bugs Everywhere.  If not, see <http://www.gnu.org/licenses/>.
 
+""" Bugs Everywhere - Compare bug reports with older revision.
+
+Usage:
+    be diff [-u] [-s ID_TYPE] [REVISION]
+    be diff [-u] [-s ID_TYPE] REPO [-- REVISION]
+
+Options:
+    -h, --help                        Show this screen.
+    -u, --uuids                       Only print the bug ids.
+    -s ID_TYPE, --subscribe=ID_TYPE   Only print changes matching ID_TYPE.
+                                      You can specify multiple ID_TYPEs as a
+                                      comma-separated list.
+                                      See `be subscribe --help` for
+                                      descriptions of ID and TYPE.
+
+ When using '-u' or '--uuids' option, the output can be piped to 'be show'
+"""
+
+from docopt import docopt
+
 import libbe
 import libbe.bugdir
 import libbe.bug
@@ -27,118 +47,94 @@ import libbe.storage
 
 import libbe.diff
 
-class Diff (libbe.command.Command):
-    __doc__ = """Compare bug reports with older tree
 
-    >>> import sys
-    >>> import libbe.bugdir
-    >>> bd = libbe.bugdir.SimpleBugDir(memory=False, versioned=True)
-    >>> io = libbe.command.StringInputOutput()
-    >>> io.stdout = sys.stdout
-    >>> ui = libbe.command.UserInterface(io=io)
-    >>> ui.storage_callbacks.set_storage(bd.storage)
-    >>> cmd = Diff()
+class Diff(libbe.command.Command):
+    # pylint: disable=missing-docstring
+    __doc__ = """ Compare bug reports with older tree
 
-    >>> original = bd.storage.commit('Original status')
-    >>> bug = bd.bug_from_uuid('a')
-    >>> bug.status = 'closed'
-    >>> changed = bd.storage.commit('Closed bug a')
-    >>> ret = ui.run(cmd, args=[original])
-    Modified bugs:
-      abc/a:cm: Bug A
-        Changed bug settings:
-          status: open -> closed
-    >>> ret = ui.run(cmd, {'subscribe':'%(bugdir_id)s:mod', 'uuids':True}, [original])
-    a
-    >>> bd.storage.versioned = False
-    >>> ret = ui.run(cmd, args=[original])
-    Traceback (most recent call last):
-      ...
-    UserError: This repository is not revision-controlled.
-    >>> ui.cleanup()
-    >>> bd.cleanup()
-    """ % {'bugdir_id':libbe.diff.BUGDIR_ID}
+        >>> import sys
+        >>> import libbe.bugdir
+        >>> bd = libbe.bugdir.SimpleBugDir(memory=False, versioned=True)
+        >>> io = libbe.command.StringInputOutput()
+        >>> io.stdout = sys.stdout
+        >>> ui = libbe.command.UserInterface(io=io)
+        >>> ui.storage_callbacks.set_storage(bd.storage)
+        >>> cmd = Diff()
+
+        >>> ui.setup_command(cmd)
+
+        >>> original = bd.storage.commit('Original status')
+        >>> bug = bd.bug_from_uuid('a')
+        >>> bug.status = 'closed'
+        >>> changed = bd.storage.commit('Closed bug a')
+        >>> cmd.run([original])
+        Modified bugs:
+          abc/a:cm: Bug A
+            Changed bug settings:
+              status: open -> closed
+        >>> cmd.run(['--subscribe=%(bugdir_id)s:mod', '--uuids', original])
+        a
+        >>> bd.storage.versioned = False
+        >>> cmd.run([original])
+        Traceback (most recent call last):
+        ...
+        UserError: This repository is not revision-controlled.
+        >>> ui.cleanup()
+        >>> bd.cleanup()
+    """ % {'bugdir_id': libbe.diff.BUGDIR_ID}
+
     name = 'diff'
 
-    def __init__(self, *args, **kwargs):
-        libbe.command.Command.__init__(self, *args, **kwargs)
-        self.options.extend([
-                libbe.command.Option(name='repo', short_name='r',
-                    help='Compare with repository in REPO instead'
-                         ' of the current repository.',
-                    arg=libbe.command.Argument(
-                        name='repo', metavar='REPO',
-                        completion_callback=libbe.command.util.complete_path)),
-                libbe.command.Option(name='subscribe', short_name='s',
-                    help='Only print changes matching SUBSCRIPTION, '
-                    'subscription is a comma-separated list of ID:TYPE '
-                    'tuples.  See `be subscribe --help` for descriptions '
-                    'of ID and TYPE.',
-                    arg=libbe.command.Argument(
-                        name='subscribe', metavar='SUBSCRIPTION')),
-                libbe.command.Option(name='uuids', short_name='u',
-                    help='Only print the changed bug UUIDS.'),
-                ])
-        self.args.extend([
-                libbe.command.Argument(
-                    name='revision', metavar='REVISION', default=None,
-                    optional=True)
-                ])
+    def run(self, args=None):
+        args = args or []
+        params = docopt(__doc__, argv=[Diff.name] + args)
 
-    def _run(self, **params):
         try:
             subscriptions = libbe.diff.subscriptions_from_string(
-                params['subscribe'])
-        except ValueError, e:
-            raise libbe.command.UserError(e.msg)
-        bugdirs = self._get_bugdirs()
-        for uuid,bugdir in sorted(bugdirs.items()):
-            self.diff(bugdir, subscriptions, params=params)
+                params['--subscribe'])
+        except ValueError as exc:
+            raise libbe.command.UserError(exc)
 
+        bugdirs = self._get_bugdirs()  # pylint: disable=no-member
+        for bugdir in sorted(bugdirs.values()):
+            self.diff(bugdir, subscriptions, params['REPO'],
+                      params['REVISION'], params['--uuids'])
 
-    def diff(self, bugdir, subscriptions, params):
-        if params['repo'] == None:
-            if bugdir.storage.versioned == False:
+    # pylint: disable=too-many-arguments
+    def diff(self, bugdir, subscriptions, repo=None, revision=None,
+             uuids=False):
+        if repo is None:
+            if not bugdir.storage.versioned:
                 raise libbe.command.UserError(
                     'This repository is not revision-controlled.')
-            if params['revision'] == None: # get the most recent revision
-                params['revision'] = bugdir.storage.revision_id(-1)
-            old_bd = libbe.bugdir.RevisionedBugDir(bugdir, params['revision'])
-        else:
-            old_storage = libbe.storage.get_storage(params['repo'])
-            old_storage.connect()
-            old_bd_current = libbe.bugdir.BugDir(old_storage, from_disk=True)
-            if params['revision'] == None: # use the current working state
-                old_bd = old_bd_current
-            else:
-                if old_bd_current.storage.versioned == False:
-                    raise libbe.command.UserError(
-                        '{} is not revision-controlled.'.format(
-                            bugdir.storage.repo))
-                old_bd = libbe.bugdir.RevisionedBugDir(old_bd_current,revision)
-        d = libbe.diff.Diff(old_bd, bugdir)
-        tree = d.report_tree(subscriptions)
 
-        if params['uuids'] == True:
+            if revision is None:  # get the most recent revision
+                revision = bugdir.storage.revision_id(-1)
+            old_bd = libbe.bugdir.RevisionedBugDir(bugdir, revision)
+        else:
+            old_storage = libbe.storage.get_storage(repo)
+            old_storage.connect()
+            # pylint: disable=unexpected-keyword-arg
+            old_bd_current = libbe.bugdir.BugDir(old_storage, from_disk=True)
+            if revision is None:  # use the current working state
+                old_bd = old_bd_current
+            elif not old_bd_current.storage.versioned:
+                msg = "Repo '%s' is not revision-controlled."
+                raise libbe.command.UserError(msg % bugdir.storage.repo)
+            else:
+                old_bd = libbe.bugdir.RevisionedBugDir(old_bd_current,
+                                                       revision)
+
+        tree = libbe.diff.Diff(old_bd, bugdir).report_tree(subscriptions)
+
+        if uuids:
             uuids = []
             bugs = tree.child_by_path('/bugs')
             for bug_type in bugs:
                 uuids.extend([bug.name for bug in bug_type])
-            print >> self.stdout, '\n'.join(uuids)
-        else :
+            print >> self.stdout, '\n'.join(uuids)  # pylint: disable=no-member
+        else:
             rep = tree.report_string()
-            if rep != None:
-                print >> self.stdout, rep
+            if rep is not None:
+                print >> self.stdout, rep  # pylint: disable=no-member
         return 0
-
-    def _long_help(self):
-        return """
-Uses the storage backend to compare the current tree with a previous
-tree, and prints a pretty report.  If REVISION is given, it is a
-specifier for the particular previous tree to use.  Specifiers are
-specific to their storage backend.
-
-Besides the standard summary output, you can use the options to output
-UUIDS for the different categories.  This output can be used as the
-input to 'be show' to get an understanding of the current status.
-"""
