15cdbf2e0499c08ccfb6a631d166bbefbf70f7c9
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2017-10-15 21:36:35 +0200

    Port help command to docopt

--- libbe/command/help.py
+++ libbe/command/help.py
@@ -20,13 +20,26 @@
 # You should have received a copy of the GNU General Public License along with
 # Bugs Everywhere.  If not, see <http://www.gnu.org/licenses/>.
 
+""" Bugs Everywhere - Print help for specified topic.
+
+Usage:
+    be help TOPIC
+    be (-h| --help)
+
+Topics:
+    repo
+    server
+"""
+
+from docopt import docopt
+
+
 import libbe
 import libbe.command
 import libbe.command.util
 
 
-TOPICS = {
-    'repo': """A BE repository containing child bugdirs
+TOPICS = {'repo': """A BE repository containing child bugdirs
 
 BE repositories are stored in an abstract `Storage` instance, which
 may or may not be versioned.  If you're using BE to track bugs in your
@@ -47,7 +60,7 @@ And the local client can run:
 
     $ be --repo http://123.123.123.123:54321 list
 
-or whichever command they like.        
+or whichever command they like.
 
 Because the storage server serves repositories at the `Storage` level,
 it can be inefficient.  For example, `be list` will have to transfer
@@ -56,8 +69,7 @@ server can also be harder to lock down, because users with write
 access can potentially store data that cannot be parsed by BE.  For a
 more efficient server, see `be serve-commands`.
 """,
-##
-    'server': """A server for remote BE command execution
+          'server': """A server for remote BE command execution
 
 The usual way for a user to interact with a BE bug tracker for a
 particular project is to clone the project repository.  They can then
@@ -81,65 +93,20 @@ locally, and then POSTed to the command server, where the command is
 executed.  The output of the command is returned to the client for
 display.  This requires much less traffic over the wire than running
 the same command via a storage server.
-""",
-        }
+"""}
 
 
-class Help (libbe.command.Command):
-    """Print help for given command or topic
-
-    >>> import sys
-    >>> import libbe.bugdir
-    >>> io = libbe.command.StringInputOutput()
-    >>> io.stdout = sys.stdout
-    >>> ui = libbe.command.UserInterface(io=io)
-    >>> cmd = Help()
-
-    >>> ret = ui.run(cmd, args=['help'])
-    usage: be help [options] [TOPIC]
-    <BLANKLINE>
-    Options:
-      -h, --help  Print a help message.
-    <BLANKLINE>
-      --complete  Print a list of possible completions.
-    <BLANKLINE>
-    <BLANKLINE>
-    Print help for specified command/topic or list of all commands.
-    """
+class Help(libbe.command.Command):
+    """ Print help for given topic. """
     name = 'help'
 
-    def __init__(self, *args, **kwargs):
-        libbe.command.Command.__init__(self, *args, **kwargs)
-        self.args.extend([
-                libbe.command.Argument(
-                    name='topic', metavar='TOPIC', default=None,
-                    optional=True,
-                    completion_callback=self.complete_topic)
-                ])
-
-    def _run(self, **params):
-        if params['topic'] == None:
-            if hasattr(self.ui, 'help'):
-                print >> self.stdout, self.ui.help().rstrip('\n')
-        elif params['topic'] in libbe.command.commands(command_names=True):
-            module = libbe.command.get_command(params['topic'])
-            Class = libbe.command.get_command_class(module,params['topic'])
-            c = Class(ui=self.ui)
-            self.ui.setup_command(c)
-            print >> self.stdout, c.help().rstrip('\n')
-        elif params['topic'] in TOPICS:
-            print >> self.stdout, TOPICS[params['topic']].rstrip('\n')
+    def run(self, args=None):
+        args = args or []
+        params = docopt(__doc__, argv=[Help.name] + args)
+        topic = params['TOPIC']
+        if topic in TOPICS:
+            # pylint: disable=no-member
+            print >> self.stdout, TOPICS[topic].rstrip('\n')
         else:
-            raise libbe.command.UserError(
-                '"%s" is neither a command nor topic' % params['topic'])
+            raise libbe.command.UserError('"%s" is an uknown topic' % topic)
         return 0
-
-    def _long_help(self):
-        return """
-Print help for specified command/topic or list of all commands.
-"""
-
-    def complete_topic(self, command, argument, fragment=None):
-        commands = libbe.command.util.complete_command()
-        topics = sorted(TOPICS.keys())
-        return commands + topics
