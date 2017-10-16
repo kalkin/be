fdc8c5f9f9b2d2d00a17ec6d47b7910dfee97fba
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2017-10-16 20:37:49 +0200

    Port show command to docopt

--- libbe/command/show.py
+++ libbe/command/show.py
@@ -20,7 +20,36 @@
 # You should have received a copy of the GNU General Public License along with
 # Bugs Everywhere.  If not, see <http://www.gnu.org/licenses/>.
 
+""" Bugs Everywhere - Show a bug, comment, or combination of both.
+
+Usage:
+    be show [-x] [--only-raw-body] [-c] [ID...]
+
+Options:
+  -c, --no-comments  Disable comment output.  This is useful if you just want
+                     more details on a bug's current status.
+  --only-raw-body    When printing only a single comment, just print it's
+                     body.  This allows extraction of non-text content types.
+  -x, --xml          Dump as XML
+
+Show all information about the bugs or comments whose IDs are given.
+If no IDs are given, show the entire repository.
+
+Without the --xml flag set, it's probably not a good idea to mix bug
+and comment IDs in a single call, but you're free to do so if you
+like.  With the --xml flag set, there will never be any root comments,
+so mix and match away (the bug listings for directly requested
+comments will be restricted to the bug uuid and the requested
+comment(s)).
+
+Directly requested comments will be grouped by their parent bug and
+placed at the end of the output, so the ordering may not match the
+order of the listed IDs.
+"""
+
 import sys
+
+from docopt import docopt
 
 import libbe
 import libbe.command
@@ -29,7 +58,7 @@ import libbe.util.id
 import libbe.version
 
 
-class Show (libbe.command.Command):
+class Show(libbe.command.Command):
     """Show a particular bug, comment, or combination of both.
 
     >>> import sys
@@ -42,7 +71,9 @@ class Show (libbe.command.Command):
     >>> ui.storage_callbacks.set_storage(bd.storage)
     >>> cmd = Show(ui=ui)
 
-    >>> ret = ui.run(cmd, args=['/a',])  # doctest: +ELLIPSIS
+    >>> ui.setup_command(cmd)
+
+    >>> ret = cmd.run(['/a',])  # doctest: +ELLIPSIS
               ID : a
       Short name : abc/a
         Severity : minor
@@ -54,7 +85,7 @@ class Show (libbe.command.Command):
     Bug A
     <BLANKLINE>
 
-    >>> ret = ui.run(cmd, {'xml':True}, ['/a'])  # doctest: +ELLIPSIS
+    >>> ret = cmd.run(['--xml', '/a'])  # doctest: +ELLIPSIS
     <?xml version="1.0" encoding="..." ?>
     <be-xml>
       <version>
@@ -78,32 +109,15 @@ class Show (libbe.command.Command):
     """
     name = 'show'
 
-    def __init__(self, *args, **kwargs):
-        libbe.command.Command.__init__(self, *args, **kwargs)
-        self.options.extend([
-                libbe.command.Option(name='xml', short_name='x',
-                                     help='Dump as XML'),
-                libbe.command.Option(name='only-raw-body',
-                    help="When printing only a single comment, just print it's"
-                  " body.  This allows extraction of non-text content types."),
-                libbe.command.Option(name='no-comments', short_name='c',
-                    help="Disable comment output.  This is useful if you just "
-                         "want more details on a bug's current status."),
-                ])
-        self.args.extend([
-                libbe.command.Argument(
-                    name='id', metavar='ID', default=None,
-                    optional=True, repeatable=True,
-                    completion_callback=libbe.command.util.complete_bug_comment_id),
-                ])
-
-    def _run(self, **params):
-        bugdirs = self._get_bugdirs()
-        if params['only-raw-body'] == True:
+    def run(self, args=None):
+        args = args or []
+        params = docopt(__doc__, argv=[Show.name] + args)
+        bugdirs = self._get_bugdirs()  # pylint: disable=no-member
+        if params['--only-raw-body']:
             if len(params['id']) != 1:
                 raise libbe.command.UserError(
                     'only one ID accepted with --only-raw-body')
-            bugdir,bug,comment = (
+            _, bug, comment = (
                 libbe.command.util.bugdir_bug_comment_from_user_id(
                     bugdirs, params['id'][0]))
             if comment == bug.comment_root:
@@ -112,67 +126,55 @@ class Show (libbe.command.Command):
                     % params['id'][0])
             sys.__stdout__.write(comment.body)
             return 0
+        # pylint: disable=no-member
         print >> self.stdout, \
-            output(bugdirs, params['id'], encoding=self.stdout.encoding,
-                   as_xml=params['xml'],
-                   with_comments=not params['no-comments'])
+            output(bugdirs, params['ID'], encoding=self.stdout.encoding,
+                   as_xml=params['--xml'],
+                   with_comments=not params['--no-comments'])
         return 0
 
-    def _long_help(self):
-        return """
-Show all information about the bugs or comments whose IDs are given.
-If no IDs are given, show the entire repository.
-
-Without the --xml flag set, it's probably not a good idea to mix bug
-and comment IDs in a single call, but you're free to do so if you
-like.  With the --xml flag set, there will never be any root comments,
-so mix and match away (the bug listings for directly requested
-comments will be restricted to the bug uuid and the requested
-comment(s)).
-
-Directly requested comments will be grouped by their parent bug and
-placed at the end of the output, so the ordering may not match the
-order of the listed IDs.
-"""
 
 def _sort_ids(bugdirs, ids, with_comments=True):
     bugs = []
     root_comments = {}
-    for id in ids:
-        p = libbe.util.id.parse_user(bugdirs, id)
+    for _id in ids:
+        p = libbe.util.id.parse_user(bugdirs, _id)
         if p['type'] == 'bug':
             bugs.append(p['bug'])
-        elif with_comments == True:
+        elif with_comments:
             if p['bug'] not in root_comments:
                 root_comments[p['bug']] = [p['comment']]
             else:
                 root_comments[p['bug']].append(p['comment'])
-    for bugname in root_comments.keys():
+    for bugname in root_comments:
         assert bugname not in bugs, \
             'specifically requested both #/%s/%s# and #/%s#' \
             % (bugname, root_comments[bugname][0], bugname)
     return (bugs, root_comments)
+
 
 def _xml_header(encoding):
     lines = ['<?xml version="1.0" encoding="%s" ?>' % encoding,
              '<be-xml>',
              '  <version>',
              '    <tag>%s</tag>' % libbe.version.version()]
-    for tag,value in sorted(libbe.version.version_info.items()):
+    for tag, value in sorted(libbe.version.version_info.items()):
         lines.append('    <%s>%s</%s>' % (tag, value, tag))
     lines.append('  </version>')
     return lines
 
+
 def _xml_footer():
     return ['</be-xml>']
 
+
 def output(bugdirs, ids, encoding, as_xml=True, with_comments=True):
-    if ids == None or len(ids) == 0:
+    if ids is None or not ids:
         ids = []
         for bugdir in bugdirs.values():
             bugdir.load_all_bugs()
             ids.extend([bug.id.user() for bug in bugdir])
-    uuids,root_comments = _sort_ids(bugdirs, ids, with_comments)
+    uuids, root_comments = _sort_ids(bugdirs, ids, with_comments)
     lines = []
     if as_xml:
         lines.extend(_xml_header(encoding))
@@ -186,8 +188,8 @@ def output(bugdirs, ids, encoding, as_xml=True, with_comments=True):
             lines.append(bug.string(show_comments=with_comments))
             if spaces_left > 0:
                 spaces_left -= 1
-                lines.append('') # add a blank line between bugs/comments
-    for bugname,comments in root_comments.items():
+                lines.append('')  # add a blank line between bugs/comments
+    for bugname, comments in root_comments.items():
         bug = libbe.command.util.bug_from_uuid(bugdirs, bugname)
         if as_xml:
             lines.extend(['  <bug>', '    <uuid>%s</uuid>' % bug.uuid])
@@ -202,7 +204,7 @@ def output(bugdirs, ids, encoding, as_xml=True, with_comments=True):
                 lines.append(comment.string())
                 if spaces_left > 0:
                     spaces_left -= 1
-                    lines.append('') # add a blank line between bugs/comments
+                    lines.append('')  # add a blank line between bugs/comments
         if as_xml:
             lines.append('</bug>')
     if as_xml:
