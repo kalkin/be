4733e7124f2aaa57b2a507cc8a3226fc3dc4d8c8
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2017-10-16 18:31:38 +0200

    Port severity command to docopt

--- libbe/command/severity.py
+++ libbe/command/severity.py
@@ -21,14 +21,31 @@
 # You should have received a copy of the GNU General Public License along with
 # Bugs Everywhere.  If not, see <http://www.gnu.org/licenses/>.
 
+""" Bugs Everywhere - Show/Set a bug's severity level.
+
+Usage:
+    be severity SEVERITY BUG_ID...
+
+ If no severity is specified, the current value is printed.  If
+ a severity level is specified, it will be assigned to the bug.
+
+ Severity levels are:
+    %s
+
+    You can override the list of allowed severities on a
+    per-repository basis.
+"""
+
+from docopt import docopt
+
 import libbe
 import libbe.bug
 import libbe.command
 import libbe.command.util
 
 
-class Severity (libbe.command.Command):
-    """Change a bug's severity level
+class Severity(libbe.command.Command):
+    """ Change a bug's severity level
 
     >>> import sys
     >>> import libbe.bugdir
@@ -39,13 +56,15 @@ class Severity (libbe.command.Command):
     >>> ui.storage_callbacks.set_storage(bd.storage)
     >>> cmd = Severity(ui=ui)
 
+    >>> ui.setup_command(cmd)
+
     >>> bd.bug_from_uuid('a').severity
     'minor'
-    >>> ret = ui.run(cmd, args=['wishlist', '/a'])
+    >>> ret = cmd.run(['wishlist', '/a'])
     >>> bd.flush_reload()
     >>> bd.bug_from_uuid('a').severity
     u'wishlist'
-    >>> ret = ui.run(cmd, args=['none', '/a'])
+    >>> ret = cmd.run(['none', '/a'])
     Traceback (most recent call last):
     UserError: Invalid severity level: none
     >>> ui.cleanup()
@@ -53,55 +72,36 @@ class Severity (libbe.command.Command):
     """
     name = 'severity'
 
-    def __init__(self, *args, **kwargs):
-        libbe.command.Command.__init__(self, *args, **kwargs)
-        self.args.extend([
-                libbe.command.Argument(
-                    name='severity', metavar='SEVERITY', default=None,
-                    completion_callback=libbe.command.util.complete_severity),
-                libbe.command.Argument(
-                    name='bug-id', metavar='BUG-ID', default=None,
-                    repeatable=True,
-                    completion_callback=libbe.command.util.complete_bug_id),
-                ])
-
-    def _run(self, **params):
+    def run(self, args=None):
+        args = args or []
+        params = docopt(__doc__ % ('\n  '.join(self.severity_levels)),
+                        argv=[Severity.name] + args)
         bugdirs = self._get_bugdirs()
-        for bug_id in params['bug-id']:
-            bugdir,bug,comment = (
+        for bug_id in params['BUG_ID']:
+            bug = (
                 libbe.command.util.bugdir_bug_comment_from_user_id(
-                    bugdirs, bug_id))
-            if bug.severity != params['severity']:
+                    bugdirs, bug_id))[1]
+            if bug.severity != params['SEVERITY']:
                 try:
-                    bug.severity = params['severity']
+                    bug.severity = params['SEVERITY']
                 except ValueError, e:
                     if e.name != 'severity':
                         raise e
-                    raise libbe.command.UserError(
-                        'Invalid severity level: %s' % e.value)
+                    msg = 'Invalid severity level: %s' % e.value
+                    raise libbe.command.UserError(msg)
         return 0
 
-    def _long_help(self):
-        try: # See if there are any per-tree severity configurations
+    @property
+    def severity_levels(self):
+        ''' Return all severity levels '''
+        try:  # See if there are any per-tree severity configurations
             bugdirs = self._get_bugdirs()
         except NotImplementedError:
-            pass # No tree, just show the defaults
+            pass  # No tree, just show the defaults
         longest_severity_len = max([len(s) for s in libbe.bug.severity_values])
         severity_levels = []
-        for severity in libbe.bug.severity_values :
+        for severity in libbe.bug.severity_values:
             description = libbe.bug.severity_description[severity]
-            s = '%*s : %s' % (longest_severity_len, severity, description)
-            severity_levels.append(s)
-        ret = """
-Show or change a bug's severity level.
-
-If no severity is specified, the current value is printed.  If a severity level
-is specified, it will be assigned to the bug.
-
-Severity levels are:
-  %s
-
-You can override the list of allowed severities on a per-repository
-basis.  See `be set --help` for details.
-""" % ('\n  '.join(severity_levels))
-        return ret
+            sev = '%*s : %s' % (longest_severity_len, severity, description)
+            severity_levels.append(sev)
+        return severity_levels
